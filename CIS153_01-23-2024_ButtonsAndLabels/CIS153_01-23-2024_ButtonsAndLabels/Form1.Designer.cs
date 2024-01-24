1213436307343440bb7e109615349da44285a149
SuperFire92 <nickdyk21@gmail.com>
2024-01-23 20:00:12 -0500

    Buttons and Label Counter
    Using buttons, add and subtract from a displayed number.

---
+++ CIS153_01-23-2024_ButtonsAndLabels/CIS153_01-23-2024_ButtonsAndLabels/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CIS153_01_23_2024_ButtonsAndLabels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_subtract = new System.Windows.Forms.Button();
+            this.lbl_num = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.Location = new System.Drawing.Point(12, 385);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(686, 191);
+            this.btn_add.TabIndex = 0;
+            this.btn_add.Text = "ADD";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_subtract
+            // 
+            this.btn_subtract.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_subtract.Location = new System.Drawing.Point(721, 385);
+            this.btn_subtract.Name = "btn_subtract";
+            this.btn_subtract.Size = new System.Drawing.Size(686, 191);
+            this.btn_subtract.TabIndex = 1;
+            this.btn_subtract.Text = "SUBTRACT";
+            this.btn_subtract.UseVisualStyleBackColor = true;
+            this.btn_subtract.Click += new System.EventHandler(this.btn_subtract_Click);
+            // 
+            // lbl_num
+            // 
+            this.lbl_num.AutoSize = true;
+            this.lbl_num.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_num.Location = new System.Drawing.Point(673, 21);
+            this.lbl_num.Name = "lbl_num";
+            this.lbl_num.Size = new System.Drawing.Size(83, 91);
+            this.lbl_num.TabIndex = 2;
+            this.lbl_num.Text = "0";
+            this.lbl_num.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1419, 588);
+            this.Controls.Add(this.lbl_num);
+            this.Controls.Add(this.btn_subtract);
+            this.Controls.Add(this.btn_add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_subtract;
+        private System.Windows.Forms.Label lbl_num;
+    }
+}
+
---
+++ CIS153_01-23-2024_ButtonsAndLabels/CIS153_01-23-2024_ButtonsAndLabels/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS153_01_23_2024_ButtonsAndLabels
+{
+    public partial class Form1 : Form
+    {
+        //Most of the time, we have private variables and public functions
+        //private - can only be accessed in the class that it belongs to
+        //public  - can be accessed in any class in the project
+
+        #region Variables
+
+        private int num;
+
+        #endregion
+
+        #region Actions
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            num = int.Parse(lbl_num.Text);
+
+            if (isEven(num))
+            {
+                changeLabelBlue();
+            }
+            else
+            {
+                changeLabelRed();
+            }
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("add");
+            num++;
+            if (num >= 100)
+            {
+                num = 0;
+            }
+            if (isEven(num))
+            {
+                changeLabelBlue();
+            }
+            else
+            {
+                changeLabelRed();
+            }
+            lbl_num.Text = num.ToString();
+        }
+
+        private void btn_subtract_Click(object sender, EventArgs e)
+        {
+            //Console.WriteLine("subtract");
+            num--;
+            if (num < 0)
+            {
+                num = 99;
+            }
+            if (isEven(num))
+            {
+                changeLabelBlue();
+            }
+            else
+            {
+                changeLabelRed();
+            }
+            lbl_num.Text = num + "";
+        }
+
+        #endregion
+
+        #region Functions
+
+        //I would NEVER code like this, good grief this is messy
+        public bool isEven(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void changeLabelBlue()
+        {
+            lbl_num.ForeColor = Color.Blue;
+        }
+
+        public void changeLabelRed()
+        {
+            lbl_num.ForeColor = Color.Red;
+        }
+
+        #endregion
+    }
+}
